5d2ee5c59a2602fc6ed81a4ed0895124f178b311
m3l <m33lis@gmail.com>
2017-02-28 22:16:59 +0200

    Calculator done with React, lodash and numeral

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ReactCalc
+
+
+git clone https://github.com/m33lis/ReactCalc.git
+cd ReactCalc
+npm install
+gulp
+
+It runs on http://localhost:9005
---
+++ src/main.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var _ = require('lodash');
+var numeral = require('numeral');
+
+
+var Calc = React.createClass({
+    handleClick: function() {
+        var num1, num2, op, res;
+
+        if (this.refs.num1 !== null && this.refs.numOne !== null) {
+            if (!_.isNaN(numeral(this.refs.num1.value)) && !_.isNaN(numeral(this.refs.num2.value))) {
+                num1 = numeral(this.refs.num1.value);
+                num2 = numeral(this.refs.num2.value);
+
+                if (this.refs.op !== null) {
+                    op = this.refs.op.value;
+                    if (op.length>0) {
+                        switch(op) {
+                            case '+':
+                                // operator +
+                                res = num1.add(num2.value()).value();
+                                break;
+                            case '-':
+                                // operator -
+                                res = num1.subtract(num2.value()).value();
+                                break;
+                            case '*':
+                                // operator *
+                                res = num1.multiply(num2.value()).value();
+                                break;
+                            case '/':
+                                // operator /, let's divide
+                                res = num1.divide(num2.value()).value();
+                                break;
+                            default:
+                                alert("ERROR: Wrong operator!", op);
+                        }
+                    } else {
+                        alert("ERROR:: Operator missing!");
+                   }
+
+                   alert("Result: "+res);
+                }
+
+            } else {
+                alert("ERROR: one of the numbers entered is NaN!");
+            }
+        }
+    },
+    render: function() {
+        return (
+            <div>
+                <h1>ReactCalc app</h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>num1</td>
+                            <td>op</td>
+                            <td>num2</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><input ref="num1" type="text" size="5" /></td>
+                            <td><input ref="op" type="text" size="2" /></td>
+                            <td><input ref="num2" type="number" size="5" /></td>
+                            <td><button onClick={this.handleClick}>Calculate!</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Calc />, document.getElementById('app'));
